ff0b0ec0a89758b9a4c8ff6cd4da346f7484be75
Merojxon <sirojiddinovmerojiddin66@gmail.com>
2023-10-20 18:17:28 +0500

    done

---
+++ main.js
@@ -0,0 +1,96 @@
+// // problem 157
+// const massive =[12,24,34,23,23,4,224]
+// const juftIndexMassive =[]
+// const toqIndexMassive =[]
+
+// for(let i=0; i<massive.length; i++){
+//     if(i%2==0){
+//         juftIndexMassive.push(massive[i])
+//     }else{
+//         toqIndexMassive.push(massive[i])
+//     }
+// }
+// console.log(juftIndexMassive);
+// console.log(toqIndexMassive);
+
+
+
+// // Problem 167
+// const massive =[12,24,34,23,23,4,224]
+// let juftIndexCount=0
+
+// for(let i=0; i<massive.length; i++){
+//     if(i%2==0){
+//         juftIndexCount+= massive[i];
+//     }
+// }
+// console.log(juftIndexCount);
+
+
+
+// // Problem180
+// const massive =[12,24,34,23,23,4,224]
+// let max;
+// let min;
+// let med;
+
+// for(let i=0; i<massive.length;i++){
+//     max=massive[i]
+//     min=massive[i]
+//     for(let j=0; j<massive.length;j++){
+//         med=massive[j]
+//         if(max<med){
+//             max=med
+//         }if(min>med){
+//             min=med
+//         }
+//     }
+// }
+// console.log("max", max);
+// console.log("min", min);
+
+// //Problem155
+// const massive =[12,24,34,23,23,4,224]
+// const juftIndexMassive =[]
+
+// for(let i=0; i<massive.length; i++){
+//     if(i%2==0){
+//         juftIndexMassive.push(massive[i])
+// }
+// console.log(juftIndexMassive);
+
+
+// // Problem156
+// const massive =[12,24,34,23,23,4,224]
+// const toqIndexMassive =[]
+
+// for(let i=0; i<massive.length; i++){
+//     if(i%2!=0){
+//         toqIndexMassive.push(massive[i])
+//     }
+// }
+// console.log(toqIndexMassive.reverse());
+
+// // Array1
+// const emptyArray = []
+// const n =+prompt('n sonini kiritng')
+// if(n<=10){
+//     for(let i=0; i<=n;i++){
+//         if(i%2!=0){
+//             emptyArray.push(i)
+//         }
+//     }
+// }
+// console.log(emptyArray);
+
+
+// // Array2
+// const emptyArray = [];
+// const n = +prompt("n sonini kiriting")
+
+// if(n<=10){
+//     for(let i=0; i<n; i++){
+//         emptyArray.push(2**i)
+//     }
+// }
+// console.log(emptyArray);
